99263307ee4c45ad2fae64af83cdf063f51b98b1
will8989 <williamlongming@gmail.com>
2018-03-28 23:50:26 +0800

    change add user link to drop down menu

--- fyp/navAdmin.php
+++ fyp/navAdmin.php
@@ -1,16 +1,22 @@
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
-        <a class="navbar-brand" href="index.html">Cashless Canteen</a>
+        <a class="navbar-brand" href="admin.php">Cashless Canteen</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="adduser.php">Add User</a>
+           <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Add User
+              </a>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+                <a class="dropdown-item" href="adduser.php">Add Student/Staff</a>
+                <a class="dropdown-item" href="adduser1.php">Add Canteen Staff</a>
+              </div>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">View User</a>
+              <a class="nav-link" href="viewuser.php">View User</a>
             </li>
              <li class="nav-item">
               <a class="nav-link" href="#">Add Credit</a>
