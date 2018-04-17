1297364227bd8706452d6382178e5d677b98b150
badman11 <100075956@students.swinburne.edu.my>
2018-04-17 22:10:41 +0800

    password hashing fucntion

---
+++ function.php
@@ -0,0 +1,10 @@
+<?php 
+function hash_password($password){
+		$salt = sha1('@#$DSFT%T%YY%^U&U&@#$C');
+		$pass = hash('sha512',$salt.$password); //SHA512 encrypted algorithm 
+		return $pass;
+}
+
+
+
+?>
