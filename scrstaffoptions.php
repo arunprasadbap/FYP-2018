1ef8453ca4c2e24d56015b09d2cf83a676317b3e
badman11 <100075956@students.swinburne.edu.my>
2018-05-05 06:17:04 +0800

    Update scrstaffoptions.php

--- scrstaffoptions.php
+++ scrstaffoptions.php
@@ -1,4 +1,7 @@
-<?php session_start();
+<?php
+ob_start();
+include_once "script/send_emailfunction.php";
+session_start();
 $host = "localhost";
 $dbusername = "root";
 $dbpassword = "";
@@ -8,13 +11,12 @@ $mysqli = new mysqli ($host, $dbusername, $dbpassword, $dbname);
 
 
 
-if(isset($_GET['idready'])){
-	
-	$id=$_GET['idready'];
-	if(mysqli_query($mysqli,"DELETE FROM orders WHERE orderid='$id'")){
-	header("location:scrStaff.php");
-	}
-	
+if (isset($_GET['idready'])){
+    $id = $_GET['idready'];
+    $unique = uniqid();
+    if(mysqli_query($mysqli, "UPDATE orders SET order_status = 'on the way', number = '$unique' WHERE orderid='$id'")){
+        header("location:scrStaff.php");
+    }
 }
 
 
@@ -38,6 +40,16 @@ $date=$row['date'];
 	
 	mysqli_query($mysqli,"DELETE FROM history WHERE historyid='$id'");
 	mysqli_query($mysqli,"DELETE FROM historyadmin WHERE id='$id'");
+	
+	//send mail
+    $fetch = mysqli_query($mysqli, "SELECT username,email FROM account WHERE userid='$userid'");
+    $row = mysqli_fetch_assoc($fetch);
+
+    $subject = "Order has been canceled";
+    $message = "<h3>Dear " . $row['username'] . "</h3><br>";
+    $message .= "<p>Sorry, your order has been canceled.</p><br><br><br>";
+    $message .= "<p>Regards,<br>Cashless Canteen</p>";
+    send_email($row['email'], $message, $subject);
 //}
 	header("location:scrStaff.php");
 	
