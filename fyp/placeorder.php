e5d3d59358b567dff056bbe3c5370df221c5832d
arunprasadbap <arunprasadbap619@gmail.com>
2018-05-12 10:59:14 +0800

    Update placeorder.php
    updated

--- fyp/placeorder.php
+++ fyp/placeorder.php
@@ -13,14 +13,20 @@ $date = date('Y/m/d H:i:s');
 $onlydat=date('Y/m/d');
 
 if(isset($_POST['proceed'])){
+	
 	$newamount=0;
 	$radio=$_POST['radio'];
 	$balance=$_POST['balance'];
 	$userid=$_SESSION['idnum'];
-	$fetch=mysqli_query($mysqli,"SELECT * FROM account WHERE userid='$userid'");
-$row=mysqli_fetch_assoc($fetch);
+	 $fetch=mysqli_query($mysqli,"SELECT * FROM account WHERE userid='$userid'");
+	$row=mysqli_fetch_assoc($fetch);
 $currentamount=$row['amount'];
+if($currentamount >= $balance ){
+	if(isset($_SESSION['alert'])){
+		unset($_SESSION['alert']);
+	}
 $newamount=$currentamount-$balance;
+mysqli_query($mysqli,"UPDATE account SET amount='$newamount' WHERE userid='$userid'");
 
 if($radio=='dinein'){
 	$tableno=$_POST['table'];
@@ -54,7 +60,21 @@ while($row=mysqli_fetch_assoc($fetch)){
 			
 	mysqli_query($mysqli,"INSERT INTO orders(date,userid,username,foodid,foodname,quantity,stall,ordertype,foodamount)VALUES('$date','$id','$name','$fid',' $fname',' $fquantity','$stalf','$tableno','$famount')");	
 	mysqli_query($mysqli,"INSERT INTO history(userid,username,amount,food,time,stall,quantity)VALUES('$id','$name','$famount',' $fname','$date','$stalf','$fquantity')");
-	
+			mysqli_query($mysqli,"INSERT INTO historyadmin(userid,username,amount,food,time,stall,quantity)VALUES('$id','$name','$famount',' $fname','$date','$stalf','$fquantity')");
+$datefound=mysqli_query($mysqli,"SELECT * FROM map WHERE date='$onlydat'");
+		if(mysqli_num_rows($datefound)==1){
+			
+		while($row=mysqli_fetch_assoc($datefound)){
+			$newamountup=$famount+$row['amount'];
+			
+		}
+mysqli_query($mysqli,"UPDATE map SET amount='$newamountup' WHERE date='$onlydat'");
+
+
+	}else{	
+		mysqli_query($mysqli,"INSERT INTO map(date,stall,amount,food,user)VALUES('$onlydat','$stalf','$famount','$fname','$id')");
+		
+	}
 	
 	mysqli_query($mysqli,"DELETE FROM cart WHERE user_id='$userid'");
 	header("location:cartselection.php");
@@ -93,30 +113,33 @@ while($row=mysqli_fetch_assoc($fetch)){
 			 $fquantity=$array[$i]['quantity'];
 			 $stalf=$array[$i]['stall'];
 			
+
+			
 		mysqli_query($mysqli,"INSERT INTO orders(date,userid,username,foodid,foodname,quantity,stall,ordertype,foodamount)VALUES('$date','$id','$name','$fid','$fname','$fquantity','$stalf','$tableno','$famount')");
 		mysqli_query($mysqli,"INSERT INTO history(userid,username,amount,food,time,stall,quantity)VALUES('$id','$name','$famount',' $fname','$date','$stalf','$fquantity')");
+			mysqli_query($mysqli,"INSERT INTO historyadmin(userid,username,amount,food,time,stall,quantity)VALUES('$id','$name','$famount',' $fname','$date','$stalf','$fquantity')");
 		$datefound=mysqli_query($mysqli,"SELECT * FROM map WHERE date='$onlydat'");
 		if(mysqli_num_rows($datefound)==1){
-			
+			echo "found";
 		while($row=mysqli_fetch_assoc($datefound)){
-			$newamountup=$newamountup+$row['amount'];
+			$newamountup=$famount+$row['amount'];
 			
 		}
 mysqli_query($mysqli,"UPDATE map SET amount='$newamountup' WHERE date='$onlydat'");
-
+echo "updated";
 
 	}else{	
 		mysqli_query($mysqli,"INSERT INTO map(date,stall,amount,food,user)VALUES('$onlydat','$stalf','$famount','$fname','$id')");
-		
+		echo "inserted";
 	}
 
 	mysqli_query($mysqli,"DELETE FROM cart WHERE user_id='$userid'");
-	mysqli_query($mysqli,"UPDATE account SET amount='$newamount' WHERE userid='$userid'");
+	
 		
-	header("location:cartselection.php");
+	
 	
 	}
-	
+	header("location:cartselection.php");
 
 }
 
@@ -128,7 +151,13 @@ mysqli_query($mysqli,"UPDATE map SET amount='$newamountup' WHERE date='$onlydat'
 		
 //}
 	
+}
+else{
+$cart='no money';
+$_SESSION['alert']=$cart;
+	header("Location:cart.php");
 	
+}
 	
 	
 	
@@ -137,8 +166,6 @@ mysqli_query($mysqli,"UPDATE map SET amount='$newamountup' WHERE date='$onlydat'
 	
 	
 }
-
-
 
 
 
@@ -159,3 +186,5 @@ mysqli_query($mysqli,"UPDATE map SET amount='$newamountup' WHERE date='$onlydat'
 
 
 ?>
+
+
