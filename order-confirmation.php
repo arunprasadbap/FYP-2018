e2f51dbb22e96bc296e41a5e942f452a6da1bec1
badman11 <100075956@students.swinburne.edu.my>
2018-05-16 03:52:16 +0800

    updated

--- order-confirmation.php
+++ order-confirmation.php
@@ -1,9 +1,10 @@
 <?php session_start();
 include_once "db.php";
 if(isset($_POST['confirm_order'])){
-    $mysqli->query("UPDATE `orders` SET `order_status` = 'delivered' WHERE `number` = '$_SESSION[number]'");
+    $ids = implode(", ",$_POST['order_id']);
+    $mysqli->query("UPDATE `orders` SET `order_status` = 'delivered' WHERE `orderid` in ($ids)");
     $_SESSION['success'] = 'Thank you for being with us';
-    unset($_SESSION['number']);
+    //unset($_SESSION['number']);
     header("Location: ../notification.php");
 }else{
     echo "invalid request";
