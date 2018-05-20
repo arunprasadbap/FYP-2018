e0cee48e8d8b735d04e91487d914cace2b11f9b7
arunprasadbap <arunprasadbap619@gmail.com>
2018-05-20 19:55:00 +0800

    Update register.php
    add user email notification,name validation and email validation

--- fyp/script/register.php
+++ fyp/script/register.php
@@ -1,13 +1,30 @@
 <?php
-
-require 'db.php';
+ob_start();
 session_start();
 
+require 'db.php';
+include_once "send_emailfunction.php";
 
-$id=$mysqli->escape_string($_POST['userid']);
-$name=$mysqli->escape_string($_POST['name']);
-$pass = $mysqli->escape_string($_POST['password']);
+include_once "function.php";
+$id=$_POST['userid'];
+$realpassword=$_POST['password'];
+$id    = $mysqli->escape_string($_POST['userid']);
+$name  = $mysqli->escape_string($_POST['name']);
+$pass  = hash_password($mysqli->escape_string($_POST['password'])); //hasing pawwsord
 $email = $mysqli->escape_string($_POST['email']);
+
+ $subject = 'Account Registration';
+   $message .= '<h3>Dear '.$name.' </h3><br>';
+    $message .='You have Successfully Registered with our system<br>';
+      $message .= '<h4>Your id is '.$id.'</h4> ';
+    $message .= '<h4>Your Password is '.$realpassword.' </h4>';
+    
+   
+        $message .= '<p><a href="https://swincashlesscanteen.000webhostapp.com/"><button>Please Click Here To Login</button></a></p><br>';
+        $message .= '<p>Regards,<br>Cashless Canteen</p>';
+       
+
+	
 
 
 
@@ -15,33 +32,42 @@ $email = $mysqli->escape_string($_POST['email']);
 //check email existance
 $result = $mysqli->query("SELECT * FROM Account WHERE userid='$id'");
 
-if ($result->num_rows > 0)
+$resultemail = mysqli_query($mysqli,"SELECT * FROM Account WHERE email='$email'");
+if (strlen($name) < 15)
 {
-echo ("<SCRIPT LANGUAGE='JavaScript'>
-    window.alert('ID already exists!')
-    window.location.href='../adduser.php';
-    </SCRIPT>");
 
-}
-else
-{
-	$sql = "INSERT INTO Account (userid,username,userpass,email) VALUES ('$id','$name','$pass','$email')";
+if(mysqli_num_rows($resultemail)==0 ){
+	    
+	    
 	
-	if ($mysqli->query($sql))
-	{
-	 echo ("<SCRIPT LANGUAGE='JavaScript'>
-    window.alert('Registration Successful!')
-    window.location.href='../adduser.php';
-    </SCRIPT>");
-		
-	}
-	else
-	{
-       echo ("<SCRIPT LANGUAGE='JavaScript'>
-    window.alert('Registration Failed, Please Try Again!')
-    window.location.href='../adduser.php';
-    </SCRIPT>");
+if ($result->num_rows > 0){
+	$_SESSION['error'] = "ID already exists!"; 
+	 
+}else{
+   
+	$sql = "INSERT INTO Account (userid,username,userpass,email,role) VALUES ('$id','$name','$pass','$email','2')";
+	
+	if ($mysqli->query($sql)==TRUE){
+	 
+	 	$_SESSION['success'] = 'Registration Successful!';
+	 	   
+	 	  send_email($email,$message, $subject);
+	 
+	}else{
+       	$_SESSION['error'] = 'Registration Failed, Please Try Again!';
 	}
 }
-
+}
+else{
+    
+  	$_SESSION['error'] = "Email Already Exists"; 
+}
+}
+else{
+  $_SESSION['error'] = "Too many characters in your name max 15";  
+    
+}
+header("Location: ../adduser.php");
+exit();
+ob_end_flush();
 ?>
