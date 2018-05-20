69c776c465129fc47231bc24615e035ace905e8c
will8989 <williamlongming@gmail.com>
2018-05-20 18:07:24 +0800

    scripts for edit and update quantity

---
+++ script/editFoodStaff.php
@@ -0,0 +1,11 @@
+
+<?php
+
+require 'db.php';
+
+
+$result = $mysqli->query("Select * FROM scrbreakfast ") or die($mysqli->error);
+
+?>
+
+
---
+++ script/update.php
@@ -0,0 +1,34 @@
+<?php
+
+require 'db.php';
+session_start();
+
+$the_item = $_GET['product_id'];
+$stock = (int)$_POST['stock'];
+
+
+
+$update = $mysqli->query("UPDATE scrbreakfast SET quantity='$stock' WHERE id='$the_item' ");
+
+	
+	if ($update)
+	{
+		echo ("<SCRIPT LANGUAGE='JavaScript'>
+    window.alert('Product Quantity Successfully Updated!')
+    window.location.href='../editFood.php';
+    </SCRIPT>");
+	}
+	else
+	{
+	echo ("<SCRIPT LANGUAGE='JavaScript'>
+    window.alert('Product Quantity Failed to update!')
+    window.location.href='../scrStaff.php';
+    </SCRIPT>");
+	}
+	
+
+
+
+
+
+?>
