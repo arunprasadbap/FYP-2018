081b5ad2e3f4d304fa05734ea371a20632720791
arunprasadbap <arunprasadbap619@gmail.com>
2018-05-22 14:15:55 +0800

    Update drinkpage.php
    updated drink page

--- fyp/drinkpage.php
+++ fyp/drinkpage.php
@@ -19,11 +19,11 @@ require 'script/db.php';
     <h1><title>Cashless Canteen</title></h1>
 
     <!-- Bootstrap core CSS -->
-	
+  
     
-	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link href="css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="styleuserfood.css">
+      <link href="css/layout.css" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/styleuserfood.css">
     <!-- Custom styles-->
     
   </head>
@@ -41,9 +41,9 @@ require 'script/db.php';
     <div class="container">
        
     
-	
+  
 
-	<?php $fetch=mysqli_query($mysqli,"SELECT * FROM scrdrinks");
+  <?php $fetch=mysqli_query($mysqli,"SELECT * FROM scrdrinks");
 while($row=mysqli_fetch_assoc($fetch)){  
 
 $id=$row['id'];
@@ -55,27 +55,30 @@ $userid=$_SESSION['idnum'];
   <a>
     <img src="images/drink/<?php echo $row['img']; ?>" id="image" class="img-responsive" alt="drink" width="300" height="200">
   </a><form action="addtocart.php" method="post">
-  <div class="desc"><h3><?php echo '<b>'.$row['drinks'].'</b>'.'&nbsp;'.'&nbsp;'.'RM'.$row['drink_amount']; ?></h3>
- <h2><input type="number"  name="quantity" min="1" max="5"oninvalid="this.setCustomValidity('Select Quatity')" oninput="setCustomValidity('')"  required></h2><input type="hidden" value="<?php  echo $id;  ?>" name="cartid" ><button type="submit" name="addcartdrink" id="button" class="btn btn-primary">
+  <div class="desc"><h6><?php echo '<b>'.$row['drinks'].'</b>'.'&nbsp;'.'&nbsp;'.'RM'.$row['drink_amount']; ?></h6>
+ <input type="hidden" value="<?php  echo $id;  ?>" name="cartid" ><button type="submit" name="addcartdrink" id="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal" >
          <span class="glyphicon glyphicon-shopping-cart"></span>  <span class="text">ADD TO CART</span>
         </button>
+  
 </form>
+
   </div>
 
 </div>
 
-<?php
+  
+  
+  
+  
+  <?php
 } ?>
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+  
+  
+  
+  
+  
+  
        
        
         
