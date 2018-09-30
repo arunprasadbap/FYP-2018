5e0a639c4c256000a58185c540c147a40d1c66ff
arunprasadbap <arunprasadbap619@gmail.com>
2018-10-01 03:25:37 +0800

    Update bfastProduct.php
    update

--- fyp/bfastProduct.php
+++ fyp/bfastProduct.php
@@ -5,7 +5,6 @@ require 'script/db.php';
 
 
 ?>
-
 <?php if(isset($_SESSION['role']) && $_SESSION['role'] ==2): ?>
  
 <html lang="en">
@@ -21,12 +20,11 @@ require 'script/db.php';
 
     <!-- Bootstrap core CSS -->
 	
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script><!-- Bootstrap core CSS -->
-   
-	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css" >
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="css/bootstrap.min.css" rel="stylesheet">
+     <link href="css/layout.css" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="css/styleuserfood.css">
     <!-- Custom styles-->
     
@@ -36,25 +34,25 @@ require 'script/db.php';
 
     <!-- Navigation -->
     <?php require 'navUser.php'; ?>
-
-
+    
+    
     <!-- Page Content -->
    <h1 class="my-4"></h1>
    
+      
     <div class="container">
-       
-    <div class="modal fade" id="myModal" role="dialog">
+        <div class="modal fade" id="myModal" role="dialog">
   <div class="modal-dialog modal-dialog-centered">
     
       <!-- Modal content-->
       <div class="modal-content">
   <div class="modal-header">
-  <h2 class="modal-title"><strong>Out Of Stock</strong></h2>
+  <h2 class="modal-title" ><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Out Of Stock</strong></h2>
   </div>
  
   <div class="modal-body">
   <div class="alert alert-danger">
-  <h4><p align="center"><strong>Sorry you cant add this item to your cart at the moment</strong></p></h4>
+  <h5><p align="center"><strong>Sorry you cant add this item to your cart at the moment</strong></p></h5>
 </div>
     
   </div>
@@ -65,7 +63,7 @@ require 'script/db.php';
   </div>
 </div>
 </div>
-
+    
 	
 
 	<?php $fetch=mysqli_query($mysqli,"SELECT * FROM scrbreakfast");
@@ -79,8 +77,9 @@ $userid=$_SESSION['idnum'];
 <div class="gallery">
   <a>
     <img src="images/breakfast/<?php echo $row['img']; ?>" id="image" class="img-responsive" alt="breakfast" width="300" height="200">
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button type="button" id="fav" onclick="select(this)" style="color:#000000;font-weight: bold;" value="<?php  echo $row['id'];?>" class="btn btn-outline-danger">Add to Favorites</button>
   </a><form action="addtocart.php" method="post">
-  <div class="desc"><h3><?php echo '<b>'.$row['breakfast'].'</b>'.'&nbsp;'.'&nbsp;'.'RM'.$row['breakfast_amount']; ?></h3>
+  <div class="desc"><h6><?php echo '<b>'.$row['breakfast'].'</b>'.'&nbsp;'.'&nbsp;'.'RM'.$row['breakfast_amount']; ?></h6>
 <input type="hidden" value="<?php  echo $id;  ?>" name="cartid" ><button type="submit" name="addcart" id="button" class="btn btn-primary">
          <span class="glyphicon glyphicon-shopping-cart"></span>  <span class="text">ADD TO CART</span>
         </button>
@@ -96,10 +95,54 @@ $userid=$_SESSION['idnum'];
 <?php
 } ?>
 	
+	<style>
+.btn { 
+
+	width:140px;
+	height:0.5px;
+	 padding: 8px 1px;
+	 font-size: 13px;
+	 margin:4px;
+    cursor: pointer;
+}
+.btn:hover {
+    background-color: #A9A9A9;
+}
+.fa fa-heart-o{
+	 color: orange;
+}
+ 
+
+</style>
 	
 	
+	<script>
+
+function select(b){
 	
+	//alert(b.value);
+	var name=b.value;
 	
+	b.style.backgroundColor = "E57474";
+	
+
+
+	
+		$.post("addfav.php",{
+			bnam:name
+			
+		}, function(data, status){
+			toastr.success(data);
+			
+		});
+
+toastr.options.newestOnTop = false;
+
+		
+	toastr.options.closeMethod = 'slideUp';
+	
+}
+</script>
 	
 	
 	
@@ -117,8 +160,15 @@ $userid=$_SESSION['idnum'];
     </footer>
 
     <!-- Bootstrap core JavaScript -->
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js">
+  </script>
+  <script src="http://cdnjs.cloudflare.com/ajax/libs/toastr.js/2.0.2/js/toastr.min.js">
+  </script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.js"></script>
+    
     <script src="jquery/jquery.min.js"></script>
     <script src="js/bootstrap.bundle.min.js"></script>
+     <script src="script/ajax-call.js"></script>
 
   </body>
 </html>
