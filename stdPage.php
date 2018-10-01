f9c1d25bd49eecb60346bf6c50b169c7f1ea8598
badman11 <100075956@students.swinburne.edu.my>
2018-10-01 15:50:00 +0800

    live search for food items

--- stdPage.php
+++ stdPage.php
@@ -78,91 +78,9 @@
     <!-- Navigation -->
     <?php require 'navUser.php'; ?>
     
-   <header>
-      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner" role="listbox">
-          <!-- Slide One - Set the background image for this slide in the line below -->
-          <div class="carousel-item active" style="background-image: url('img/slideshow.jpg');background-repeat: no-repeat; background-size: cover;">
-            <div class="carousel-caption d-none d-md-block">
-              <h3>Food Example 1</h3>
-              <p>Image Example 1</p>
-            </div>
-          </div>
-          <!-- Slide Two - Set the background image for this slide in the line below -->
-          <div class="carousel-item" style="background-image: url('img/slideshow2.jpg');background-repeat: no-repeat; background-size: cover;">
-            <div class="carousel-caption d-none d-md-block">
-              <h3>Food Example 2</h3>
-              <p>Image Example 2</p>
-            </div>
-          </div>
-          <!-- Slide Three - Set the background image for this slide in the line below -->
-          <div class="carousel-item" style="background-image: url('img/slideshow3.jpg');background-repeat: no-repeat; background-size: cover;">
-            <div class="carousel-caption d-none d-md-block">
-              <h3>Food Example 3</h3>
-              <p>Image Example 3</p>
-            </div>
-          </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-    </header>
+  
 
-    <div class="container">
-      <form method="post" action="live_search_result.php">
-        <div class="row">
-            <div id="custom-search-input">
-              <div class="input-group col-md-12">
-                <!-- <form method="post" action="#"> -->
-                  <input type="text" class="search-query form-control" id="tags" name="tags" placeholder="Search for your food items" />
-                  <span class="input-group-btn">
-                      <button class="btn btn-primary btn-sm" name="search" value="search" type="submit">
-                          Search
-                      </button>
-                  </span>
-                <!-- </form> -->
-              </div>
-            </div>
-        </div>
-      </form>
-    </div>
-
-    <!-- Page Content -->
-    <div class="container">
-
-      <h1 class="my-4">Welcome to Swinburne Cafeteria</h1>
-
-      
-      <div class="row">
-        <div class="col-lg-6 mb-6">
-          <div class="card h-100">
-            <h4 class="card-header">Foods</h4>
-            <div class="card-body">
-              <a href="foodIndex.php"><img class="card-img-top" src="img/food1.jpg" alt=""></a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-6 mb-6">
-          <div class="card h-100">
-            <h4 class="card-header">Drinks</h4>
-            <div class="card-body">
-              <a href="drinkProduct.php"><img class="card-img-top" src="img/drink1.jpg" alt=""></a>
-            </div>
-          </div>
-        </div>
-        </div>
-      </div>
+   
   <hr>
     <!-- /.container -->
 
