01bcddaac448b328b4ac578b4e6522cba7f68a7f
badman11 <100075956@students.swinburne.edu.my>
2018-10-01 15:47:30 +0800

    adding live search in user home page

--- stdPage.php
+++ stdPage.php
@@ -1,6 +1,19 @@
 <?php session_start(); ?>
 <?php if(isset($_SESSION['role']) && $_SESSION['role'] ==2 ): ?>
-    <?php require 'script/bal.php'; ?>
+    <?php require 'script/bal.php'; 
+
+      $queryString = "SELECT `name` FROM `food_listing` ORDER BY `name`";
+
+      $result = mysqli_query($mysqli,$queryString);
+
+      $arrayNameList = array();
+
+      while($rows = mysqli_fetch_assoc($result)){
+          $arrayNameList[] = $rows['name'];
+      }  
+
+      $arrayNameString = '"'.implode('","',$arrayNameList).'"';
+    ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,6 +32,46 @@
     <!-- Custom styles-->
     <link href="css/layout.css" rel="stylesheet">
   </head>
+  <style>
+    #custom-search-input {
+        margin:0;
+        margin-top: 10px;
+        padding: 0;
+        width: 100%;
+    }
+ 
+    #custom-search-input .search-query {
+        padding-right: 3px;
+        padding-right: 4px \9;
+        padding-left: 3px;
+        padding-left: 4px \9;
+        /* IE7-8 doesn't have border-radius, so don't indent the padding */
+ 
+        margin-bottom: 0;
+        -webkit-border-radius: 3px;
+        -moz-border-radius: 3px;
+        border-radius: 3px;
+    }
+ 
+    #custom-search-input button {
+            border: 0;
+            padding: 2px 25px;
+            margin-top: 0px;
+            position: relative;
+            left: -2px;
+            margin-bottom: 0;
+            -webkit-border-radius: 3px;
+            -moz-border-radius: 3px;
+            border-radius: 3px;
+            color: #ffff;
+            background-color: #0062cc;
+    }
+ 
+    .search-query:focus + button {
+        z-index: 3;   
+    }
+
+  </style>
 
   <body>
 
@@ -66,6 +119,24 @@
       </div>
     </header>
 
+    <div class="container">
+      <form method="post" action="live_search_result.php">
+        <div class="row">
+            <div id="custom-search-input">
+              <div class="input-group col-md-12">
+                <!-- <form method="post" action="#"> -->
+                  <input type="text" class="search-query form-control" id="tags" name="tags" placeholder="Search for your food items" />
+                  <span class="input-group-btn">
+                      <button class="btn btn-primary btn-sm" name="search" value="search" type="submit">
+                          Search
+                      </button>
+                  </span>
+                <!-- </form> -->
+              </div>
+            </div>
+        </div>
+      </form>
+    </div>
 
     <!-- Page Content -->
     <div class="container">
@@ -106,6 +177,43 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.bundle.min.js"></script>
     <script src="script/ajax-call.js"></script>
+
+
+     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+     <script>
+    $( function(){
+      var  availableTags = [<?php echo $arrayNameString ?>];
+        // var availableTags = [
+        //   "ActionScript",
+        //   "AppleScript",
+        //   "Asp",
+        //   "BASIC",
+        //   "C",
+        //   "C++",
+        //   "Clojure",
+        //   "COBOL",
+        //   "ColdFusion",
+        //   "Erlang",
+        //   "Fortran",
+        //   "Groovy",
+        //   "Haskell",
+        //   "Java",
+        //   "JavaScript",
+        //   "Lisp",
+        //   "Perl",
+        //   "PHP",
+        //   "Python",
+        //   "Ruby",
+        //   "Scala",
+        //   "Scheme"
+        // ];
+        
+        $( "#tags" ).autocomplete({
+          source: availableTags
+        });
+  });
+  </script
 
   </body>
 
