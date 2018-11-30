878125d175e6fb83e25c77bac9384d5fe92ae786
will8989 <williamlongming@gmail.com>
2018-11-30 09:30:11 +0800

    view in detail of suggestion form for admin

---
+++ Suggestion form2.php
@@ -0,0 +1,86 @@
+<?php session_start();
+
+?>
+<?php if(isset($_SESSION['role']) && ($_SESSION['role'] ==1) ): ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>View Suggestion Form</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+  
+   <!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="css/layout1.css" rel="stylesheet">
+        <!-- Custom styles for this template -->
+        <link href="css/custom.css" rel="stylesheet">
+
+        <!-- Website Font style -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+    <!-- Google Fonts -->
+        <link href='https://fonts.googleapis.com/css?family=Passion+One' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
+<body>
+ <?php require 'navAdmin.php'; ?>
+  <?php require 'script/viewSuggestScript2.php';?>
+<br><br>
+
+    <div id="background-carousel">
+   <div class="login-form">
+    <form action="#" method="#">
+        <h2 class="text-center">View Suggestion Form</h2>   
+        <?php while($list = $result->fetch_assoc()):?>    
+        <div class="form-group">
+            <input type="text" class="form-control" name="name" value="<?php echo $list['Name']; ?>"readonly>
+        </div>
+        <div class="form-group">
+            <input type="text" class="form-control" name="email" value="<?php echo $list['Email']; ?>"readonly>
+        </div>
+         <div class="form-group">
+            <input type="text" class="form-control" name="phone" value="<?php echo $list['Phone']; ?>"readonly>
+        </div>
+        <div class="form-group">
+           <textarea class="input100" name="message" readonly><?php echo $list['Message']; ?></textarea>
+        </div>
+        
+    <?php endwhile;?>
+    </form>
+</div>
+
+    
+   
+<div id="dropDownSelect1"></div>
+
+
+  <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+
+  <script src="vendor/animsition/js/animsition.min.js"></script>
+
+  <script src="vendor/bootstrap/js/popper.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+  <script src="vendor/select2/select2.min.js"></script>
+
+  <script src="vendor/daterangepicker/moment.min.js"></script>
+  <script src="vendor/daterangepicker/daterangepicker.js"></script>
+
+  <script src="vendor/countdowntime/countdowntime.js"></script>
+
+  <script src="js/main2.js"></script>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.bundle.min.js"></script>
+
+
+
+
+</body>
+</html>
+<?php else:   
+header("Location: index.php");
+exit();?>
+<?php endif; ?> 
